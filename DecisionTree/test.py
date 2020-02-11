1c62d6b8e377c6e0c827b7937dc966defabfbcee
Hyphi <nrechati@student.42.fr>
2020-02-11 13:56:24 +0100

    [Decision Tree] Clean sources

--- DecisionTree/test.py
+++ DecisionTree/test.py
@@ -6,7 +6,7 @@
 #    By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/11 12:53:35 by nrechati          #+#    #+#              #
-#    Updated: 2020/02/11 13:31:34 by nrechati         ###   ########.fr        #
+#    Updated: 2020/02/11 13:54:44 by nrechati         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,10 +19,14 @@ from sklearn.datasets import load_iris
 from DecisionTreeClassifier import DecisionTreeClassifier
 
 def main():
-	# Get iris dataset
+	# Get the dataset
+	print(f'{Fore.CYAN}Getting dataset from scikit-learn.org', end="")
 	iris = load_iris()
 	X = pd.DataFrame(iris.data)
 	y = pd.DataFrame(iris.target)
+	print(f'\t✔ Ok')
+
+	print(f'{Fore.MAGENTA}Setting up dataset for classifier', end="")
 	# Split Train/Test set
 	X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1)
 
@@ -35,19 +39,27 @@ def main():
 	X_test = X_test.values
 	y_test = y_test.values
 	test_data = np.concatenate((X_test, y_test), axis=1)
+	print(f'\t✔ Ok')
 
 	#Data set parameters
 	features = ["sepal_L", "sepal_W", "petal_L", "petal_W"]
 	labels = ["Setosa\t", "Versicolour", "Virginica"]
 
+	print(f'{Fore.YELLOW}Decision tree initialization', end="")
 	#Create Decision trees
 	MyTree = DecisionTreeClassifier(features, labels)
+	print(f'\t\t✔ Ok')
+	print(f'{Fore.GREEN}Building the tree', end="")
 	MyTree.root = MyTree.fit(train_data)
+	print(f'\t\t\t✔ Ok{Fore.RED}')
 
+	input("\nPress Enter to continue...")
+	print(f'\n{Fore.CYAN} Generated Tree')
 	#Print the tree
 	MyTree.print_tree(MyTree.root)
 
 	#Predict test set
+	print(f'\n{Fore.CYAN} Prediciton on test set')
 	for row in test_data:
 		prediction = MyTree.classify(row, MyTree.root)
 		print (f"{Fore.YELLOW}Actual: %s\t{Fore.GREEN}Predicted: %s{Style.RESET_ALL}" % (labels[int(row[-1])], prediction))
